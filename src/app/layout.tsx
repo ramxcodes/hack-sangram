dee380f2b31a6b7af15626a364da2b5131d58d82
Ramkrishna Swarnkar <128958601+ramxcodes@users.noreply.github.com>
2025-01-06 19:38:55 +0530

    Add Vercel Analytics for improved tracking and update package dependencies

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import localFont from "next/font/local";
+import { Analytics } from "@vercel/analytics/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -42,11 +43,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-        <body
-          className={`${geistSans.variable} ${geistMono.variable} ${Wotfard.variable} antialiased`}
-        >
-          {children}
-        </body>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} ${Wotfard.variable} antialiased`}
+      >
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
